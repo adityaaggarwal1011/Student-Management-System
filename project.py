5f59cf195e58f1cce9958cd623e5f69838ad195b
adityaaggarwal1011 <harekrishna10177@gmail.com>
2026-06-04 17:03:11 +0530

    changes

--- project.py
+++ project.py
@@ -1,7 +1,55 @@
-with open("student.txt","r") as file:
-    lines= file.readlines()
+import csv
+def main():
+    get_data()
+    add_data()
 
-for line in lines:
-    print(line.rstrip())
 
-print(type(lines))
+
+
+
+
+
+
+def get_data():
+    students = []
+    with open("students.csv") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            students.append({"ID":row["ID"],
+                            "Name":row["Name"],
+                            "Age":row["Age"],
+                            "Gender":row["Gender"],
+                            "Contact":row["Contact"],
+                            "Class":row["Class"]
+    })
+    return students
+
+
+
+def add_data():
+    new_student = input("Enter the following:\nID, Name, Age, Gender, Contact, Class\n")
+    with open("students.csv","a") as file:
+        file.write(new_student)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
