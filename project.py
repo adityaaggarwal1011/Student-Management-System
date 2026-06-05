fcb8e2a9e01916c6b7e9e259944b9ab40b23914c
adityaaggarwal1011 <harekrishna10177@gmail.com>
2026-06-05 11:25:51 +0530

    ok

--- project.py
+++ project.py
@@ -1,10 +1,9 @@
 import csv
 def main():
-    delete_data()
+    sort_by_age()
+    
 
-
-
-
+#Getting the data from csv
 def get_data():
     students = []
     with open("students.csv") as file:
@@ -19,14 +18,14 @@ def get_data():
     })
     return students
 
-
-
+#Adding new student to csv
 def add_data():
     new_student = input("Enter the following:\nID, Name, Age, Gender, Contact, Class\n")
     with open("students.csv","a") as file:
         file.write(new_student)
 
 
+#Deleting a student from csv
 def delete_data():
     students = get_data()
     delete_id = input("Enter ID to delete: ")
@@ -40,6 +39,51 @@ def delete_data():
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(students)
+
+
+
+
+
+#Find by ID
+def find_by_id():
+    students = get_data()
+    unique_id= input("Enter the unique ID: ")
+    for student in students:
+        if student["ID"] == unique_id:
+            print(student)
+            break
+
+#Find by name
+def find_by_name():
+    students = get_data()
+    unique_name= input("Enter the unique name: ")
+    for student in students:
+        if student["Name"] == unique_name:
+            print(student)
+    
+#Find by class
+def find_by_class():
+    students = get_data()
+    grade= input("Enter the class: ")
+    for student in students:
+        if student["Class"] == grade:
+            print(student)
+    
+
+#Sort by age
+def sort_by_age():
+    students = get_data()
+    students.sort(key=lambda student: int(student["Age"]))    
+    for student in students:
+        print(student, "\n".strip())
+
+
+
+
+
+
+
+
 
 
 
